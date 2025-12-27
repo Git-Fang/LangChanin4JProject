12f9273a05d5592d3ee2b0b6d433bf8ee6db84b4
Git-Fang <2419766853@qq.com>
2025-12-27 10:44:57 +0800

    提交2--20251226: 默认功能为个人助手。

--- src/main/java/org/fb/service/assistant/ChatAssistant.java
+++ src/main/java/org/fb/service/assistant/ChatAssistant.java
@@ -1,10 +1,12 @@
 package org.fb.service.assistant;
 
 import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 
 public interface ChatAssistant {
     String chat(String userMessage);
 
+    @SystemMessage(fromResource = "defaultPersonalPrompt.txt")
     public String chat(@MemoryId long memoryId, @UserMessage String userMessage);
 }
