75d6aa42f044b13ca7cf4aa4b4b02b4c288f9e7d
Git-Fang <2419766853@qq.com>
2025-12-31 14:22:09 +0800

    提交2--20251230：tools工具调整

--- src/main/java/org/fb/tools/CommonTools.java
+++ src/main/java/org/fb/tools/CommonTools.java
@@ -9,10 +9,12 @@ import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingSearchRequest;
 import dev.langchain4j.store.embedding.EmbeddingSearchResult;
 import dev.langchain4j.store.embedding.EmbeddingStore;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class CommonTools {
 
     @Autowired
@@ -22,10 +24,11 @@ public class CommonTools {
     private EmbeddingStore<TextSegment> embeddingStore;
 
 
-    @Tool(name = "查询向量数据库信息", value="根据用户输入信息从pinecone向量数据库中查询后，并返回给用户")
+    @Tool(name = "查询qdrant向量数据信息", value="根据用户输入信息从qdrant向量数据库中查询并返回")
     public String embeddingSearch(@P(value="用户问题", required = true) String question) {
         //1.1提问，并将问题转成向量数据
         Embedding queryEmbedding = embeddingModel.embed(question).content();
+
         //1.2创建搜索请求对象
         EmbeddingSearchRequest searchRequest = EmbeddingSearchRequest.builder()
                 .queryEmbedding(queryEmbedding)
@@ -35,14 +38,10 @@ public class CommonTools {
 
         //2.根据搜索请求 searchRequest 在向量存储中进行相似度搜索
         EmbeddingSearchResult<TextSegment> searchResult = embeddingStore.search(searchRequest);
-        //searchResult.matches()：获取搜索结果中的匹配项列表。
-        //.get(0)：从匹配项列表中获取第一个匹配项
         EmbeddingMatch<TextSegment> embeddingMatch = searchResult.matches().get(0);
 
-        //3.1获取匹配项的相似度得分
-        System.out.println("答案相似度："+embeddingMatch.score()); // 0.8144288515898701
-        //3.2返回文本结果
-        System.out.println("答案："+embeddingMatch.embedded().text());
+        //3.获取匹配项的相似度得分与答案
+        log.info("答案相似度score：{}; 结果为：{}",embeddingMatch.score(),embeddingMatch.embedded().text());
 
         return embeddingMatch.embedded().text();
     }
