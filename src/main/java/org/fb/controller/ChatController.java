3c1ab7cd01c8c5610dda243df5f26e5774f20769
Git-Fang <2419766853@qq.com>
2026-01-04 00:11:12 +0800

    提交2--20260103：增加智能对话2接口

--- src/main/java/org/fb/controller/ChatController.java
+++ src/main/java/org/fb/controller/ChatController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @Tag(name = "智能对话")
 @RestController
 @RequestMapping("/xiaozhi")
@@ -28,6 +30,15 @@ public class ChatController {
 
         // 使用AI理解用户意图后执行业务
         return chatService.chat(memoryId, userMessage);
+    }
+
+
+    @PostMapping("/chat2")
+    @Operation(summary = "智能对话2")
+    public String chat(@RequestBody String userMessage) {
+
+        // 使用AI理解用户意图后执行业务
+        return chatService.chat(new Random().nextLong(), userMessage);
     }
 
 }
