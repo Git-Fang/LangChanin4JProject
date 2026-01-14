64defaa47e9adae4984101d35179472396a1a277
Git-Fang <2419766853@qq.com>
2026-01-14 12:24:00 +0800

    2026-01-13: 批量文件操作增加完成时间

--- src/main/java/org/fb/constant/BusinessConstant.java
+++ src/main/java/org/fb/constant/BusinessConstant.java
@@ -3,8 +3,10 @@ package org.fb.constant;
 
 public class BusinessConstant {
 
-
-    public static final String TEMP_FILE_PATH = "D:\\个人资料\\uploaded_documents\\";
+    private static final String OS = System.getProperty("os.name").toLowerCase();
+    private static final boolean IS_WINDOWS = OS.contains("win");
+    
+    public static final String TEMP_FILE_PATH = IS_WINDOWS ? "D:\\个人资料\\uploaded_documents\\" : "/tmp/uploaded_documents/";
 
     public static final String BAIDU_MAP_MCP_SERVER = "@baidumap/mcp-server-baidu-map";
 
