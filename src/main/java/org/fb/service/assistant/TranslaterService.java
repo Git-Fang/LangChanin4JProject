cf29c6ac46d064d8ead0c9a7678cbd54f96d6099
Git-Fang <2419766853@qq.com>
2026-01-14 22:32:33 +0800

    2026-01-14: 翻译过程仅过滤查询TERMS类型向量数据

--- src/main/java/org/fb/service/assistant/TranslaterService.java
+++ src/main/java/org/fb/service/assistant/TranslaterService.java
@@ -9,22 +9,23 @@ import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 
 /*
-* chatModel = "ollamaChatModel",表示使用ollama模型
-* */
+ * chatModel = "ollamaChatModel",表示使用ollama模型
+ * 使用translationTermExtractor替代termExtractionAgent，确保翻译过程中提取的术语不会被存入向量数据库
+ * */
 @AiService(wiringMode = AiServiceWiringMode.EXPLICIT,
         chatModel = "chatModel",
-        tools = {"mongoDBTools", "termExtractionAgent",  "commonTools"},
+        tools = {"mongoDBTools", "translationTermExtractor", "commonTools"},
         chatMemoryProvider = "chatMemoryProvider"
 
 )
 public interface TranslaterService {
 
     @SystemMessage(fromResource = "translate-prompt.txt")
-    @UserMessage("结合向量数据中type为TERMSle的相似向量数据完成该次翻译：{{userMessage}}")
+    @UserMessage("请基于向量数据中type为TERMS的过滤查询相似向量数据完成该次翻译：{{userMessage}}")
     String translate(String userMessage);
 
     @SystemMessage(fromResource = "translate-prompt.txt")
-    @UserMessage("结合向量数据中type为TERMSle的相似向量数据完成该次翻译：{{userMessage}}")
+    @UserMessage("请基于向量数据中type为TERMS的过滤查询相似向量数据完成该次翻译：{{userMessage}}")
     String translate(@MemoryId long memoryId,  @V("userMessage") String userMessage);
 
 }
