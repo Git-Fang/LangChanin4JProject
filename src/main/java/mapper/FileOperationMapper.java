d3dff6f99570171981bc0c05fc186bf6fc5399f7
Git-Fang <2419766853@qq.com>
2026-02-02 20:16:23 +0800

    2026-02-01：清除master分支内容

--- src/main/java/mapper/FileOperationMapper.java
+++
@@ -1,9 +0,0 @@
-package mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.fb.bean.FileOperation;
-
-@Mapper
-public interface FileOperationMapper extends BaseMapper<FileOperation> {
-}
--- src/main/java/org/fb/service/ChatService.java
+++
@@ -1,7 +0,0 @@
-package org.fb.service;
-
-public interface ChatService {
-
-    String chat(Long memoryId, String message);
-
-}
